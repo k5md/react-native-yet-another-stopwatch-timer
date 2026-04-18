41aaa3c35e200ad4a042d1a5fed43c70ba66764d
k5md <serg.dfc@yandex.ru>
2026-04-18 20:29:58 +0300

    add a sanity check that example project renders without errors

---
+++ example/__tests__/App.jsx
@@ -0,0 +1,23 @@
+import { render, screen, fireEvent, waitFor, act } from '@testing-library/react-native';
+import React from 'react';
+import App from '../App';
+
+beforeEach(() => {
+  jest.useFakeTimers();
+  jest.spyOn(console, 'warn').mockImplementation(() => {});
+  jest.spyOn(console, 'log').mockImplementation(() => {});
+});
+
+afterEach(async () => {
+  jest.restoreAllMocks();
+  await act(async () => {
+    jest.runOnlyPendingTimers();
+  });
+  jest.useRealTimers();
+});
+
+test('renders', async () => {
+  render(<App />);
+  jest.advanceTimersByTime(1000);
+
+});
